e4d06c0d20a71369a61e7507719b3ea329f6d031
Vanderlei José Alamino <valamino@mtrix.com.br>
2025-06-23 13:54:58 -0300

    tela de login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
+import { Login } from './ui/pages/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: Login },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+];
---
+++ frontend/src/app/ui/pages/login/login.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
+  templateUrl: './login.html',
+  styleUrl: './login.scss'
+})
+export class Login {
+  // Dados do formulário
+  protected username = '';
+  protected password = '';
+  protected errorMessage = '';
+
+  // Credenciais mockadas
+  private readonly MOCK_USER = 'cleide';
+  private readonly MOCK_PASS = 'revir321$';
+
+   protected hidePassword = true;
+
+  protected onSubmit(): void {
+    if (this.username === this.MOCK_USER && this.password === this.MOCK_PASS) {
+      // Login bem-sucedido (simulado)
+      this.errorMessage = '';
+      alert('Login bem-sucedido!'); // Pode substituir por navegação
+    } else {
+      this.errorMessage = 'Usuário ou senha incorretos';
+    }
+  }
+}
